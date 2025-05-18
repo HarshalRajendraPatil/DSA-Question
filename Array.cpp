233c4a81f54e719c4a0f3b4698ddfcb3bb96d276
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2025-05-18 10:56:49 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -42,3 +42,53 @@ class Solution {
         return ans;
     }
 };
+
+// Q4. Given an array arr[], check whether it is sorted in non-decreasing order. Return true if it is sorted otherwise false.
+
+class Solution {
+  public:
+    bool arraySortedOrNot(vector<int>& arr) {
+        for(int i = 0; i < arr.size() - 1; i++){
+            if (arr[i] > arr[i + 1]) return false;
+        }
+        return true;
+    }
+};
+
+// Q5. Given a sorted array arr and an integer k, find the position(0-based indexing) at which k is present in the array using binary search.
+
+class Solution {
+  public:
+    int binarysearch(vector<int> &arr, int k) {
+        // code here
+        int l = 0;
+        int r = arr.size() - 1;
+        int pos = -1;
+        while(l <= r){
+            int mid = (l + (r-l)) / 2;
+            if(arr[mid] == k) {
+                pos = mid;
+                break;
+            }
+            else if(arr[mid] < k) l = mid + 1;
+            else r = mid - 1;
+        }
+        return pos;
+    }
+};
+
+// Q6. Given an array arr[] of size n, filled with numbers from 1 to n-1 in random order. The array has only one repetitive element. Your task is to find the repetitive element.
+
+class Solution {
+  public:
+    int findDuplicate(vector<int>& arr) {
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < arr.size(); i++){
+            mpp[arr[i]]++;
+        }
+        for(auto it: mpp){
+            if(it.second > 1) return it.first;
+        }
+        return -1;
+    }
+};
