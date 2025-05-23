de4bdbc25d8f0801ca8d061f9a18fff8b075d20e
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2025-05-23 20:02:15 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -233,3 +233,16 @@ void maximumAdjacent(int sizeOfArray, int arr[]) {
         cout << max(arr[i], arr[i+1]) << " ";
     }
 }
+
+// Q14. Given an integer k and array arr. Your task is to return the position of the first occurrence of k in the given array and if element k is not present in the array then return -1.
+
+class Solution {
+  public:
+    int search(int k, vector<int>& arr) {
+        for (int i = 0; i < arr.size(); i++){
+            if(arr[i] == k)return i+1;
+        }
+        return -1;
+    }
+};
+
