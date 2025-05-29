dd49d204de4935aaf2c85c7f79284483b16f87b9
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2025-05-29 17:20:31 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -427,3 +427,17 @@ class Solution {
         return tsum - sum;
     }
 };
+
+
+// Q25. Given a sorted array arr[] of n positive integers having all the numbers occurring exactly twice, except for one number which will occur only once. Find the number occurring only once.
+
+class Solution {
+  public:
+    int search(int n, int arr[]) {
+        int num = 0;
+        for(int i = 0; i < n; i++){
+            num ^= arr[i];
+        }
+        return num;
+    }
+};
