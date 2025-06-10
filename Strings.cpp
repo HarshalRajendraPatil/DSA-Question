cc8698952de070041a191f1fee1d4b829eda3fd6
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2025-06-10 19:30:14 +0530

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -52,3 +52,79 @@ class Solution {
         return s;
     }
 };
+
+
+// Q4. Given a string s consisting of lowercase English Letters. Return the first non-repeating character in s.
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        unordered_map<char,int>mpp;
+        set<char>se;
+        for(char c:s){
+            mpp[c]++;
+        }
+        for(auto it: mpp){
+            if(it.second == 1) se.insert(it.first);
+        }
+        for(char c:s){
+            if(se.count(c)) return c;
+        }
+        return '$';
+    }
+};
+
+
+// Q5. Given a string s which may contain lowercase and uppercase characters. The task is to remove all duplicate characters from the string and find the resultant string. The order of remaining characters in the output should be same as in the original string.
+
+class Solution {
+  public:
+    string removeDuplicates(string &s) {
+        // code here
+        string res = "";
+        vector<int> vect(256, 0);
+        for(char c:s){
+            vect[c - '0'] = 1;
+        }
+        for(char c : s){
+            if(vect[c - '0'] == 1){
+                res+=c;
+                vect[c - '0'] = 0;
+            }
+        }
+        return res;
+    }
+};
+
+
+// Q6. You are given a string s , the task is to reverse the string using stack.
+
+class Solution {
+  public:
+    string reverse(const string& s) {
+        stack<char> st;
+        string res = "";
+        for(auto c : s){
+            st.push(c);
+        }
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        return res;
+    }
+};
+
+
+// Q7. You are given a string s, consisting of lowercase alphabets. Your task is to remove consecutive duplicate characters from the string. 
+
+class Solution {
+  public:
+    string removeConsecutiveCharacter(string& s) {
+        string res = "";
+        for(int i = 0; i <= s.size() - 1; i++){
+            if(s[i] != s[i + 1]) res += s[i];
+        }
+        return res;
+    }
+};
