0c3744dc40f7cc25e3b185709f299b6c6cd4d76f
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2025-08-06 18:50:38 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -74,3 +74,62 @@ vector<string> generate(int n) {
     }
     return v;   
 }
+
+
+// Q4. You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> dq;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            // Remove elements outside the window
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+            
+            // Remove smaller elements from back
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            
+            // Add current element
+            dq.push_back(i);
+            
+            // Add maximum to result if window is complete
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+// Q5. Given a queue q containing integer elements, your task is to reverse the queue.
+
+class Solution {
+  public:
+    queue<int> reverseQueue(queue<int> &q) {
+        stack<int> s;
+    
+        // Transfer all elements to stack
+        while (!q.empty()) {
+            s.push(q.front());
+            q.pop();
+        }
+        
+        // Transfer back to queue (this reverses the order)
+        while (!s.empty()) {
+            q.push(s.top());
+            s.pop();
+        }
+        return q;
+    }
+};
+
+
+// Q6. 
