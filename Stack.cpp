aa2bca1bf7338fa8858b862c0296935c534cbe5e
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-01-09 11:52:49 +0530

    Add nextGreaterElement function in Stack.cpp
    Implemented a function to find the next greater element for each element in nums1 based on nums2.

--- Stack.cpp
+++ Stack.cpp
@@ -1,3 +1,28 @@
+// The next greater element of some element x in an array is the first greater element that is to the right of x in the same array. You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2. For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1. Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int>mpp;
+        stack<int>st;
+        for(int i = nums2.size() - 1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums2[i]) st.pop();
+            if (st.empty()){
+                mpp[nums2[i]] = -1;
+            }else {
+                mpp[nums2[i]] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+        vector<int> res;
+        for(int num : nums1){
+            res.push_back(mpp[num]);
+        }
+        return res;
+    }
+};
+
+
 // You are given a valid prefix expression consisting of binary operators and single-character operands. Your task is to convert it into a valid postfix expression.
 
 class Solution {
