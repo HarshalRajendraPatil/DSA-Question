e04f68650ee80b3b17ad1e6b1e690b57d9a59ada
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-01-25 09:51:53 +0530

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,3 +1,41 @@
+// A celebrity is a person who is known to all but does not know anyone at a party. A party is being organized by some people. A square matrix mat[][] of size n*n is used to represent people at the party such that if an element of row i and column j is set to 1 it means ith person knows jth person. You need to return the index of the celebrity in the party, if the celebrity does not exist, return -1.
+
+class Solution {
+  public:
+    int celebrity(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int person = -1;
+        
+        for(int i = 0; i < n; i++){
+            bool isCelebrity = true;
+            for(int j = 0; j < n; j ++){
+                if (i != j && mat[i][j] == 1){
+                    isCelebrity = false;
+                    break;
+                }
+            }
+            if (isCelebrity){
+                person = i;
+                break;
+            }
+        }
+        
+        if (person == -1) return -1;
+        
+        bool isCelebrity = true;
+        for(int i = 0; i < n; i++){
+            if (mat[i][person] != 1){
+                isCelebrity = false;
+                break;
+            }
+        }
+        
+        return isCelebrity ? person : -1;
+    }
+};
+
+
 // LFU Cache
 
 struct Node {
