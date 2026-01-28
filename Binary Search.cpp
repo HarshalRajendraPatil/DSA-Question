18c7ac3d2644cdf266049316935673333911658d
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-01-28 09:44:25 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,3 +1,26 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int idx = n;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid]>=target){
+                idx = mid;
+                high = mid - 1;
+            }else {
+                low = mid+1;
+            }
+        }
+        return idx;
+    }
+};
+
+
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
 class Solution {
