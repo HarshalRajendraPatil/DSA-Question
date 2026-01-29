04c0d9e3574e52e0f8d19a5b002404565c21558b
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-01-29 09:36:12 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,3 +1,35 @@
+// There is an integer array nums sorted in ascending order (with distinct values). Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2]. Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target)return mid;
+            
+            if (nums[low] <= nums[mid]){
+                if (target >= nums[low] && target <= nums[mid]){
+                    high = mid - 1;
+                }else {
+                    low = mid + 1;
+                }
+            }else{
+                if (target <= nums[high] && target >= nums[mid]){
+                    low = mid + 1;
+                }else {
+                    high = mid - 1;
+                }
+            }
+            
+        }
+        return -1;
+    }
+};
+
+
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 class Solution {
