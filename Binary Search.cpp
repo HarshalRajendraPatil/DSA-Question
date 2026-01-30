f364f87445b9eadff146dbd92c46916f1fe50d93
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-01-30 14:31:30 +0530

    Add lower bound function for sorted array
    Implemented a lower bound algorithm to find the first index in a sorted array where the value is greater than or equal to a given key.

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,3 +1,26 @@
+// Given a sorted array of nums and an integer x, write a program to find the lower bound of x. The lower bound algorithm finds the first and smallest index in a sorted array where the value at that index is greater than or equal to a given key i.e. x. If no such index is found, return the size of the array.
+
+class Solution{
+public:
+    int lowerBound(vector<int> &nums, int x){
+        int n = nums.size();
+        int low = 0;
+        int ans = n;
+        int high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] >= x){
+                ans = mid;
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+
 // There is an integer array nums sorted in ascending order (with distinct values). Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2]. Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
 class Solution {
