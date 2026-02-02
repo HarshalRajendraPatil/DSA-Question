298e2c4d08600450fbf8a02c3b92a2cb3aafa4bf
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-02-02 11:26:13 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,3 +1,28 @@
+// A peak element is an element that is strictly greater than its neighbors. Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks. You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n= nums.size();
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n-1] > nums[n-2]) return n-1;
+        int low = 1;
+        int high = n-2;
+        while(low<=high){
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            if (nums[mid-1] < nums[mid]){
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+
 // Given a sorted array of nums and an integer x, write a program to find the upper bound of x. The upper bound of x is defined as the smallest index i such that nums[i] > x. If no such index is found, return the size of the array.
 
 class Solution{
