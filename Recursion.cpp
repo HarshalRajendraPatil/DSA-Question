1c49cadeeb99bc3b9112d08cf76ffed3ff84864c
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-02-16 20:58:25 +0530

    Update Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,3 +1,42 @@
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int startIdx = 0;
+        int n = s.size();
+        int sign = 1; 
+
+        while(startIdx < n && s[startIdx] == ' ') startIdx++;
+        if (startIdx == n) return 0;
+        if (s[startIdx] == '-'){
+            sign = -1;
+            startIdx++;
+        } else if (s[startIdx] == '+') {
+            startIdx++;
+        }
+
+        long num = 0; 
+
+        while(startIdx < n){
+            if (s[startIdx] < '0' || s[startIdx] > '9') {
+                break;
+            }
+
+            int digit = s[startIdx] - '0';
+            if (num > INT_MAX / 10 || (num == INT_MAX / 10 && digit > 7)) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            num = num * 10 + digit;
+            startIdx++;
+        }
+
+        return (int)(num * sign);
+    }
+};
+
+
 // You are given a stack of integers. Your task is to reverse the stack using recursion. You may only use standard stack operations (push, pop, top/peek, isEmpty). You are not allowed to use any loop constructs or additional data structures like arrays or queues.
 
 class Solution {
