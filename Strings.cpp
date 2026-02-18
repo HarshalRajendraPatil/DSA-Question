220694ef3992975eff9c6ff7b5009ac2e3ecc582
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-02-18 20:09:28 +0530

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -1,3 +1,36 @@
+// Return a string of the words in reverse order concatenated by a single space.
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string>st;
+        string temp = "";
+        for(char c: s){
+            if (c == ' '){
+                if (temp != "") st.push(temp);
+                temp = "";
+                continue;
+            }
+            temp += c;
+        }
+        if (temp != ""){
+            st.push(temp);
+            temp = "";
+        }
+        while(!st.empty()){
+            string res = st.top();
+            st.pop();
+            if (!st.empty()){
+                temp = temp + res + " ";
+            }else {
+                temp = temp + res;
+            }
+        }
+        return temp;
+    }
+};
+
+
 // Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
 
 class Solution {
