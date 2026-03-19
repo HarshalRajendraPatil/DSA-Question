6e964f76e754b324e2d143fdc7810687859292aa
HARSHAL RAJENDRA PATIL <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-03-19 15:40:39 +0530

    Update trees.cpp

--- trees.cpp
+++ trees.cpp
@@ -1,3 +1,29 @@
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (root == nullptr) return {};
+        queue<TreeNode*> q;
+        vector<vector<int>> res;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> temp;
+            for(int i = 0; i < n; i++){
+                TreeNode* top = q.front();
+                q.pop();
+                temp.push_back(top->val);
+                if (top->left != nullptr) q.push(top->left);
+                if (top->right != nullptr) q.push(top->right);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
+
+
 // Given a binary tree with root node. Return the In-order,Pre-order and Post-order traversal of the binary tree.
 
 void getInOrder(vector<int>& res, TreeNode* root){
