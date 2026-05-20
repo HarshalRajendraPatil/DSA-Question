31cef01c47c977784d04f5eef72856918b41d66c
HARSHAL RAJENDRA PATIL <102664407+HarshalRajendraPatil@users.noreply.github.com>
2026-05-20 12:09:35 +0530

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,3 +1,50 @@
+// Word Ladder II
+class Solution {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>st(wordList.begin(), wordList.end());
+        queue<vector<string>>q;
+        q.push({beginWord});
+        vector<string>used;
+        used.push_back(beginWord);
+        int level = 0;
+        vector<vector<string>>ans;
+
+        while(!q.empty()){
+            vector<string>vec = q.front();
+            q.pop();
+            if (vec.size() > level){
+                level++;
+                for(auto word: used){
+                    st.erase(word);
+                }
+            }
+
+            string word = vec.back();
+            if (word == endWord){
+                if (ans.size() == 0) ans.push_back(vec);
+                else if (ans[0].size() == vec.size()) ans.push_back(vec); 
+                continue;
+            }
+
+            for(int i = 0; i < word.size(); i++){
+                char ori = word[i];
+                for(char c = 'a'; c <= 'z'; c++){
+                    word[i] = c;
+                    if (st.find(word) != st.end()){
+                        vec.push_back(word);
+                        q.push(vec);
+                        used.push_back(word);
+                        vec.pop_back();
+                    }
+                }
+                word[i] = ori;
+            }
+        }
+        return ans;
+    }
+};
+
 // Shortest path in Directed Acyclic Graph
 class Solution {
   public:
